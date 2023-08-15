44512a616186009af53288adcd585d2cbbd2a694
luigizampol <luigizampol@gmail.com>
2023-08-15 18:13:13 -0300

    Atualização de comentários do código
    Code comments update

--- atualizar_dados_esocial.py
+++ atualizar_dados_esocial.py
@@ -9,8 +9,6 @@ from IPython.display import display
 import os
 from datetime import datetime
 
-
-
 # Definir as opções do Microsoft Edge
 # Define Microsoft Edge options
 edge_options = webdriver.EdgeOptions()
@@ -31,36 +29,35 @@ login_button = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, '.br-button.sign-in.large'))
 )
 
-
 # Clicar no botão de login
 # Click the login button
 login_button.click()
 
 
-# Esperar até que o link seja carregado
-# Wait until the link is loaded
-certificado_link = WebDriverWait(driver, 10).until(
+# Aguardar o botão de login via certificado digital carregar
+# Waiting for the digital certificate login button to load.
+login_button_certificate = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, 'button[tabindex="4"]'))
 )
 
-# Clicar no botão de login
-# Click the login button
-certificado_link.click()
+# Clicar no botão de login via certificado digital
+# Click the digital certificate login button
+login_button_certificate.click()
 wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
 
 # Abre a planilha de colaboradores
 # Open the collaborators spreadsheet
-funcionarios = pd.read_excel("colaboradores.xlsx")
+employees = pd.read_excel("colaboradores.xlsx") # Inserir o nome do arquivo da planilha aqui. A planilha deve estar na mesma pasta do script.
 
 # Cria um data frame com os dados da planilha
 # Create a DataFrame with spreadsheet data
-funcionarios_df = pd.DataFrame(funcionarios)
+employees_df = pd.DataFrame(employees)
 
 # Cria uma lista com todos os números de contrato
 # Create a list of all contract numbers
-lista_contratos = funcionarios_df['contrato'].tolist()
+employees_contract_numbes = employees_df['contrato'].tolist()
 
 # Url base de acesso à pagina de informações contratuais
 # Base URL for accessing contractual information page
@@ -69,17 +66,21 @@ url_base = "https://www.esocial.gov.br/portal/Trabalhador/ConsultaCompleto?tsv=F
 
 # Url que será a soma da URL principal com o número do contrato de cada funcionário
 # URL that combines the base URL with each employee's contract number
-url_completa = ""
+url_with_contract_number = ""
 
 # List of URLs that the program will access and gather data from
-lista_urls = list()
+url_list = list()
 
 # Para cada número de contrato, cria o url completo e salva na lista
 # For each contract number, create the complete URL and save it to the list
-for contrato in lista_contratos:
-    url_completa = url_base + str(contrato)
-    lista_urls.append(url_completa)
+for contract_number in employees_contract_numbes:
+    url_with_contract_number = url_base + str(contract_number)
+    url_list.append(url_with_contract_number)
 
+
+
+# Dicionário onde as informações serão armazenadas
+# Dictionary where the information will be stored
 infos = {'contrato': [],
               'situacao': [],
               'cpf': [],
@@ -110,14 +111,14 @@ infos = {'contrato': [],
               'cnpj_sindicato': [],
               }
 
-# Define uma função para extrair dados com valor de um elemento
-# Define a function to extract data with value from an element
-def extrair_dados_valor(dado, id):
+# Define uma função para extrair dados com valor de um elemento pelo caminho XPATH
+# Define a function to extract data with value from an element by XPATH
+def extract_data(dado, id):
     infos[dado].append(driver.find_element(By.XPATH, f"//input[@id='{id}']").get_attribute("value"))
 
 # Define uma função para extrair dados com a opção selecionada de um elemento de menu suspenso
 # Define a function to extract data with selected option from a dropdown element
-def extrair_dados_option(dado, id):
+def extract_data_from_dropdown_menu(dado, id):
     elemento = driver.find_elements(By.CSS_SELECTOR, f"#{id} option[selected='selected']")
     if len(elemento) > 0:
         valor = elemento[0].text
@@ -127,56 +128,72 @@ def extrair_dados_option(dado, id):
 
 contador = 0
 
-for url in lista_urls:
-    driver.get(url)
+# Laço para extrair informações de cada url da variável url_list
+# Loop to extract information from each URL in the url_list variable
+for url in url_list:
+    # Navega até a url
+    # Navigate to the URL
+    driver.get(url) 
+
+    # Verifica se o funcionário está ativo e adiciona a informação ao dicionário
+    # Check if the employee is active and add the information to the dictionary
     ativo = driver.find_elements(By.CSS_SELECTOR, 'td.center[data-col="Situação"]')
     infos['situacao'].append(ativo[0].text)
+    
+    # Aguarda até que o botão para acessar as outras informações esteja disponível e clica nele
+    # Wait until the button to access other information is available and click on it
     dados_botao = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.item-menu-interno.dados-contratuais')))
     dados_botao.click()
-    extrair_dados_valor('contrato', 'idContrato')
-    extrair_dados_valor('cpf', 'cpfTrabalhador')
-    extrair_dados_valor('nome', 'nomeTrabalhador')
-    extrair_dados_valor('tipo_registro', 'TipoRegistroAdmissao')
-    extrair_dados_valor('matricula', 'InfoVinculo_Matricula')
-    extrair_dados_option('regime_trab', 'InfoVinculo_TipoRegimeTrabalhista')
-    extrair_dados_option('categoria', 'InfoVinculo_InfoContrato_CodigoCategoria')
-    extrair_dados_option('regime_prev', 'InfoVinculo_TipoRegimePrevidenciario')
-    extrair_dados_valor('nome_cargo', 'InfoVinculo_InfoContrato_NomeCargo')
-    extrair_dados_option('cbo_cargo', 'InfoVinculo_InfoContrato_CBOCargo')
-    extrair_dados_valor('nome_funcao', 'InfoVinculo_InfoContrato_NomeFuncao')
-    extrair_dados_option('cbo_funcao', 'InfoVinculo_InfoContrato_CBOFuncao')
-    extrair_dados_option('un_pagamento', 'InfoVinculo_InfoContrato_Remuneracao_UnidadeSalarioFixo')
-    extrair_dados_valor('salario_base', 'InfoVinculo_InfoContrato_Remuneracao_ValorSalarioFixo')
-    extrair_dados_valor('desc_salario_variavel', 'InfoVinculo_InfoContrato_Remuneracao_DescricaoSalarioVariavel')
-    extrair_dados_option('tipo_contrato_trabalho', 'InfoVinculo_InfoContrato_Duracao_TipoContrato')
-    extrair_dados_valor('horas_semanais', 'InfoVinculo_InfoContrato_HorarioContratual_QuantidadeHorasSemanal')
-    extrair_dados_option('tipo_jornada', 'InfoVinculo_InfoContrato_HorarioContratual_TipoJornada')
-    extrair_dados_option('tempo_parcial', 'InfoVinculo_InfoContrato_HorarioContratual_JornadaTempoParcial')
-    extrair_dados_valor('desc_jornada', 'InfoVinculo_InfoContrato_HorarioContratual_DescricaoJornada')
-    extrair_dados_valor('data_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_DataAdmissao')
-    extrair_dados_option('tipo_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_TipoAdmissao')
-    extrair_dados_option('indicativo_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_IndicativoAdmissao')
-    extrair_dados_option('regime_jornada', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_TipoRegimeJornada')
-    extrair_dados_option('natureza_atividade', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_NaturezaAtividade')
-    extrair_dados_option('mes_database', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_DataBase')
-    extrair_dados_valor('cnpj_sindicato', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_CnpjSindicatoCategoriaProfissional')
+
+    # Funções que extraem os dados
+    # Functions that extract the data
+    extract_data('contrato', 'idContrato')
+    extract_data('cpf', 'cpfTrabalhador')
+    extract_data('nome', 'nomeTrabalhador')
+    extract_data('tipo_registro', 'TipoRegistroAdmissao')
+    extract_data('matricula', 'InfoVinculo_Matricula')
+    extract_data_from_dropdown_menu('regime_trab', 'InfoVinculo_TipoRegimeTrabalhista')
+    extract_data_from_dropdown_menu('categoria', 'InfoVinculo_InfoContrato_CodigoCategoria')
+    extract_data_from_dropdown_menu('regime_prev', 'InfoVinculo_TipoRegimePrevidenciario')
+    extract_data('nome_cargo', 'InfoVinculo_InfoContrato_NomeCargo')
+    extract_data_from_dropdown_menu('cbo_cargo', 'InfoVinculo_InfoContrato_CBOCargo')
+    extract_data('nome_funcao', 'InfoVinculo_InfoContrato_NomeFuncao')
+    extract_data_from_dropdown_menu('cbo_funcao', 'InfoVinculo_InfoContrato_CBOFuncao')
+    extract_data_from_dropdown_menu('un_pagamento', 'InfoVinculo_InfoContrato_Remuneracao_UnidadeSalarioFixo')
+    extract_data('salario_base', 'InfoVinculo_InfoContrato_Remuneracao_ValorSalarioFixo')
+    extract_data('desc_salario_variavel', 'InfoVinculo_InfoContrato_Remuneracao_DescricaoSalarioVariavel')
+    extract_data_from_dropdown_menu('tipo_contrato_trabalho', 'InfoVinculo_InfoContrato_Duracao_TipoContrato')
+    extract_data('horas_semanais', 'InfoVinculo_InfoContrato_HorarioContratual_QuantidadeHorasSemanal')
+    extract_data_from_dropdown_menu('tipo_jornada', 'InfoVinculo_InfoContrato_HorarioContratual_TipoJornada')
+    extract_data_from_dropdown_menu('tempo_parcial', 'InfoVinculo_InfoContrato_HorarioContratual_JornadaTempoParcial')
+    extract_data('desc_jornada', 'InfoVinculo_InfoContrato_HorarioContratual_DescricaoJornada')
+    extract_data('data_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_DataAdmissao')
+    extract_data_from_dropdown_menu('tipo_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_TipoAdmissao')
+    extract_data_from_dropdown_menu('indicativo_adm', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_IndicativoAdmissao')
+    extract_data_from_dropdown_menu('regime_jornada', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_TipoRegimeJornada')
+    extract_data_from_dropdown_menu('natureza_atividade', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_NaturezaAtividade')
+    extract_data_from_dropdown_menu('mes_database', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_DataBase')
+    extract_data('cnpj_sindicato', 'InfoVinculo_InformacoesRegimeTrabalhista_InformacoesTrabalhadorCeletista_CnpjSindicatoCategoriaProfissional')
+    # Contador para acompanhar o progresso do script
+    # Counter to track the progress of the script
     contador += 1
-    print(f'Extraindo dados... [{contador}/{len(lista_urls)}]')
-    
+    print(f'Extraindo dados... [{contador}/{len(url_list)}]')
+
+# Transforma o dicionário em data frame
+# Convert the dictionary into a data frame
 infos_df = pd.DataFrame(infos)
 display(infos_df)
 
-#########################
 
-# Dataframes
-df1 = funcionarios_df
-df2 = infos_df
-df1['contrato'] = df1['contrato'].astype('int64')
-df2['contrato'] = df2['contrato'].astype('int64')
+# Converte os dataframes em int64
+# Convert the data frames to int64
+employees_df['contrato'] = employees_df['contrato'].astype('int64')
+infos_df['contrato'] = infos_df['contrato'].astype('int64')
 
 
-
-if 'contrato' in df1.columns and 'contrato' in df2.columns:
+# Compara as informações da planilha e atualiza somente com os dados que foram atualizados
+# Compare the information from the spreadsheet and update only with the data that has been updated
+if 'contrato' in employees_df.columns and 'contrato' in infos_df.columns:
     # Atualizando as colunas em comum
     # Updating the common columns
     colunas_em_comum = [
@@ -187,7 +204,7 @@ if 'contrato' in df1.columns and 'contrato' in df2.columns:
     'regime_jornada', 'natureza_atividade', 'mes_database', 'cnpj_sindicato'
     ]
 
-    df1.update(df2[colunas_em_comum])
+    employees_df.update(infos_df[colunas_em_comum])
 
 
 ###########################
@@ -197,26 +214,26 @@ if 'contrato' in df1.columns and 'contrato' in df2.columns:
 # Transform the old spreadsheet into a backup file
 # Replace with the name of your Excel file
 
-nome_arquivo_antigo = "colaboradores.xlsx"
+old_file_name = "colaboradores.xlsx"
 
 # Obter a data atual no formato dia_mes_ano
 # Get the current date in the format day_month_year
-data_atual = datetime.now().strftime("%d_%m_%Y")
+current_date = datetime.now().strftime("%d_%m_%Y")
 
 # Criar o novo nome do arquivo no formato "colaboradores_backup_dia_mes_ano.xlsx"
-# Create the new file name in the format "colaboradores_backup_day_month_year.xlsx"
+# Create the new file name in the format "employees_backup_day_month_year.xlsx"
 
-novo_nome_arquivo = f"colaboradores_backup_{data_atual}.xlsx"
+new_file_name = f"colaboradores_backup_{current_date}.xlsx"
 
 # Renomear o arquivo
 # Rename the file
 try:
-    os.rename(nome_arquivo_antigo, novo_nome_arquivo)
-    print(f"O arquivo {nome_arquivo_antigo} foi renomeado para {novo_nome_arquivo} com sucesso!")
+    os.rename(old_file_name, new_file_name)
+    print(f"O arquivo {old_file_name} foi renomeado para {new_file_name} com sucesso!") # File {file name} successfully renamed.
 except FileExistsError:
-    print(f"Já existe um arquivo com o nome {novo_nome_arquivo}. Por favor, escolha outro nome.")
+    print(f"Já existe um arquivo com o nome {new_file_name}. Por favor, escolha outro nome.") # There is already a file named {new_file_name}. Please choose another name.
 except FileNotFoundError:
-    print(f"O arquivo {nome_arquivo_antigo} não foi encontrado.")
+    print(f"O arquivo {old_file_name} não foi encontrado.") # The file {old_file_name} was not found
 
 # Cria um objeto ExcelWriter usando a biblioteca openpyxl
 # Create an ExcelWriter object using the openpyxl library
@@ -224,7 +241,7 @@ writer = pd.ExcelWriter('colaboradores.xlsx', engine='openpyxl')
 
 # Converte o DataFrame em uma planilha do Excel e adiciona ao objeto ExcelWriter
 # Converts the DataFrame into an Excel sheet and adds it to the ExcelWriter object
-df1.to_excel(writer, sheet_name='Funcionarios', index=False)
+employees_df.to_excel(writer, sheet_name='Funcionarios', index=False)
 
 # Salva o arquivo Excel
 # Save the file
