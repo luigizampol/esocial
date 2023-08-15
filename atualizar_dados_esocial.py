59c2c7d0e8d144e7ad941a2d1fcb8ae325c34383
luigizampol <luigizampol@gmail.com>
2023-08-15 18:29:42 -0300

    Comments update

--- atualizar_dados_esocial.py
+++ atualizar_dados_esocial.py
@@ -1,4 +1,5 @@
 # Importar as bibliotecas necessárias
+# Import the necessary libraries
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
